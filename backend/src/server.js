ac34cd32c0196aee6fa329cbcbe9f60f7111a066
OjalSingh <ojals007@gmail.com>
2026-06-25 14:26:23 +0530

    signup controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ import express from 'express'; // Import the Express library
 import dotenv from 'dotenv'; // Import the dotenv library to load environment variables
 import authRoutes from './routes/auth.route.js'; // Import the auth routes
 import messageRoutes from './routes/message.route.js'; // Import the message routes
+import { connectDB } from './lib/db.js'; // Import the database connection function
+
 
 dotenv.config(); // Load environment variables from .env file
 
@@ -9,12 +11,14 @@ dotenv.config(); // Load environment variables from .env file
 const app = express(); // Create an instance of the Express application
 const PORT = process.env.PORT || 3000; // Get the port from environment variables or default to 3000
 
+app.use(express.json()); // Middleware to parse JSON request bodies
 
 
 app.use("/api/auth", authRoutes); // Use the auth routes for routes starting with /api/auth
 app.use("/api/messages", messageRoutes); // Use the message routes for routes starting with /api/messages
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB(); // Connect to the database when the server starts
 });
 
 
