ee0ea5466706171a7cecd5d65a931d0cbea497ca
OjalSingh <ojals007@gmail.com>
2026-06-25 15:31:24 +0530

    login & logout endpoints

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -57,3 +57,44 @@ export const signup = async  (req, res) => {
     }
         
 };
+
+export const login = async (req, res) => {
+    const { email, password } = req.body; 
+
+    if(!email || !password) {
+        return res.status(400).json({ message: 'Please provide all required fields' });
+        
+    } 
+    try {  
+        const user =await User.findOne({ email });
+
+        if(!user) {
+            return res.status(400).json({ message: 'Invalid Credentials' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: 'Invalid Credentials' });
+        }
+        if(user && isPasswordValid) {
+            generateToken(user.id, res);
+        }
+
+        res.status(200).json({
+            _id: user.id,
+            username: user.username,
+            email: user.email
+        });
+
+    } catch (error) {
+        console.error("Error in login controller:", error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+
+
+export const logout = (_, res) => {
+    res.cookie('jwt', '', { maxAge: 0 }); // Clear the JWT cookie by setting its value to an empty string and maxAge to 0
+    res.status(200).json({ message: 'Logged out successfully' });
+};            
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -2,13 +2,21 @@ import jwt from 'jsonwebtoken'; // Import the jsonwebtoken library for generatin
 
 // Function to generate a JWT token for a user
 export const generateToken = (userId, res) => {
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, { 
-        expiresIn: '7d' }); // Generate a token with the user's ID, secret key, and expiration time of 7 days
+    const JWT_SECRET = process.env.JWT_SECRET; // Get the secret key from environment variables
+
+    if(!JWT_SECRET) {
+        throw new Error('JWT_SECRET is not defined in environment variables');
+    }
+
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {  expiresIn: '7d' }); // Generate a token with the user's ID, secret key, and expiration time of 7 days
+    
     res.cookie('jwt', token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,    //ms
         httpOnly: true, 
         sameSite: 'strict', // Set the cookie to be sent only in requests from the same site, cross-site requests will not include the cookie
         secure: process.env.NODE_ENV === 'production' // Set the cookie to be sent only over HTTPS in production, for development it can be false
-    }); // Set the token as an HTTP-only cookie, only doing development for now, in production it should be set to true to ensure the cookie is only sent over HTTPS
+    }); 
+    // Set the token as an HTTP-only cookie, only doing development for now, in production it should be set to true to ensure the cookie is only sent over HTTPS
+    
     return token; // Return the generated token
 }
