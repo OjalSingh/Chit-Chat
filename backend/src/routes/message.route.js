896b644a595e9b71ebc3cb9dcc61f0016f560373
OjalSingh <ojals007@gmail.com>
2026-06-28 02:35:43 +0530

    Add message schema and routes for chat functionality

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,13 +1,20 @@
 import express from 'express';
+import { getAllFriends, getMessagesByUserId, sendMessage, getChatPartners} from '../controllers/message.controller.js';
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js"
 
 const router = express.Router();
 
-router.get('/send', (req, res) => {
-    res.send('Send message route');
-});
+// the middleware execute in order - so requests ge rate limited first, then authenticated.
+router.use(arcjetProtection,protectRoute);
+router.get('/friends',   getAllFriends);
+//need to execute in this order
+router.get('/chats',  getChatPartners);  //people u have chats with
+router.get('/:id',   getMessagesByUserId); // get all messages with that id
+router.post('/send/:id',  sendMessage);
 
-router.get('/receive', (req, res) => {
-    res.send('Receive message route');
-});
+// router.get('/receive', (req, res) => {
+//     res.send('Receive message route');
+// });
 
 export default router;
